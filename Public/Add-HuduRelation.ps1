6c5d7af45ff8241a388f8a2fb3be241f7bb72670
Mendy Green <greenlighttec@users.noreply.github.com>
2025-05-15 10:22:27 -0400

    Update Add-HuduRelation.ps1
    Fixed error on initial load by adding context aware scirpt runs

--- Public/Add-HuduRelation.ps1
+++ Public/Add-HuduRelation.ps1
@@ -1,7 +1,12 @@
+# Check if this is a direct run, and load the logs if so the first time.
+if (-not ($FirstTimeLoad -eq 1)) {
     if (-not $MatchedPasswords) {$MatchedPasswords = (Get-Content -path "$MigrationLogs\Passwords.json" | ConvertFrom-json -depth 100) }
     if (-not $MatchedAssetPasswords) {$MatchedAssetPasswords = (Get-Content -path "$MigrationLogs\AssetPasswords.json" | ConvertFrom-json -depth 100) }
     if (-not $MatchedArticles) {$MatchedArticles = (Get-Content -path "$MigrationLogs\Articles.json" | ConvertFrom-json -depth 100) }
     if (-not $MatchedCompanies) {$MatchedCompanies = (Get-Content -path "$MigrationLogs\Companies.json" | ConvertFrom-json -depth 100) }
+    # Set the context so logs don't run again unless the powershell window gets closed.
+    $FirstTimeLoad = 1
+}
 
 function Add-HuduRelation {
     param(
