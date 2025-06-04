221eb1990d69f88b04c7a598ecb0cee3875bdcdc
masonville17 <anoniboni9876@gmail.com>
2025-06-04 16:30:43 +0000

    this should take care of the retry errors (we see more now that api is faster).

---
+++ Public/Invoke-HuduRequest.ps1
@@ -0,0 +1,113 @@
+function Invoke-HuduRequest {
+    <#
+    .SYNOPSIS
+    Main Hudu API function
+
+    .DESCRIPTION
+    Calls Hudu API with token
+
+    .PARAMETER Method
+    GET,POST,DELETE,PUT,etc
+
+    .PARAMETER Path
+    Path to API endpoint
+
+    .PARAMETER Params
+    Hashtable of parameters
+
+    .PARAMETER Body
+    JSON encoded body string
+
+    .PARAMETER Form
+    Multipart form data
+
+    .EXAMPLE
+    Invoke-HuduRequest -Resource '/api/v1/articles' -Method GET
+    #>
+    [CmdletBinding()]
+    Param(
+        [Parameter()]
+        [string]$Method = 'GET',
+
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string]$Resource,
+
+        [Parameter()]
+        [hashtable]$Params = @{},
+
+        [Parameter()]
+        [string]$Body,
+
+        [Parameter()]
+        [hashtable]$Form
+    )
+
+    $HuduAPIKey = Get-HuduApiKey
+    $HuduBaseURL = Get-HuduBaseURL
+
+    # Assemble parameters
+    $ParamCollection = [System.Web.HttpUtility]::ParseQueryString([String]::Empty)
+
+    # Sort parameters
+    foreach ($Item in ($Params.GetEnumerator() | Sort-Object -CaseSensitive -Property Key)) {
+        $ParamCollection.Add($Item.Key, $Item.Value)
+    }
+
+    # Query string
+    $Request = $ParamCollection.ToString()
+
+    $Headers = @{
+        'x-api-key' = (New-Object PSCredential 'user', $HuduAPIKey).GetNetworkCredential().Password;
+    }
+
+    if (($Script:Int_HuduCustomHeaders | Measure-Object).count -gt 0){
+        
+        foreach($Entry in $Int_HuduCustomHeaders.GetEnumerator()) {
+            $Headers[$Entry.Name] = $Entry.Value
+        }
+    }
+
+    $ContentType = 'application/json; charset=utf-8'
+
+    $Uri = '{0}{1}' -f $HuduBaseURL, $Resource
+    # Make API call URI
+    if ($Request) {
+        $UriBuilder = [System.UriBuilder]$Uri
+        $UriBuilder.Query = $Request
+        $Uri = $UriBuilder.Uri
+    }
+    Write-Verbose ( '{0} [{1}]' -f $Method, $Uri )
+
+    $RestMethod = @{
+        Method      = $Method
+        Uri         = $Uri
+        Headers     = $Headers
+        ContentType = $ContentType
+    }
+
+    if ($Body) {
+        $RestMethod.Body = $Body
+        Write-Verbose $Body
+    }
+
+    if ($Form) {
+        $RestMethod.Form = $Form
+        Write-Verbose ( $Form | Out-String )
+    }
+
+    try {
+        $Results = Invoke-RestMethod @RestMethod
+    } catch {
+        if ("$_".trim() -eq 'Retry later' -or "$_".trim() -eq 'The remote server returned an error: (429) Too Many Requests.') {
+            Write-Information 'Hudu API Rate limited. Waiting 30 Seconds then trying again'
+            Start-Sleep 30
+            # Retry the original REST call, not the whole wrapper function
+            $Results = Invoke-RestMethod @RestMethod
+        } else {
+            Write-Error "'$_'"
+        }
+    }
+
+    $Results
+}
