4745dc1635f74f05fdc3792d365ea331490c6413
masonville17 <anoniboni9876@gmail.com>
2025-06-23 18:58:32 +0000

    copying images to safe short path first!

--- Public/Normalize-And-ConvertImage.ps1
+++ Public/Normalize-And-ConvertImage.ps1
@@ -1,3 +1,5 @@
+[string]$WorkingDirectory = "/tmp/safeimages"
+
 function Get-ImageType {
     param (
         [string]$FilePath
@@ -16,46 +18,54 @@ function Normalize-And-ConvertImage {
     )
 
     $original = $InputPath
+    $originalExt = [IO.Path]::GetExtension($InputPath)
+    $originalName = [IO.Path]::GetFileNameWithoutExtension($InputPath)
 
-    # If no extension, guess and copy with guessed extension
-    if ((Get-Item -Path $InputPath).Extension -eq '') {
-        $Magick = New-Object ImageMagick.MagickImage($InputPath)
-        $guessedExt = $Magick.Format.ToString().ToLower()
-        $InputPath += ".$guessedExt"
-        Copy-Item -Path $original -Destination $InputPath -Force
+    if (-not (Test-Path $WorkingDirectory)) {
+        New-Item -ItemType Directory -Path $WorkingDirectory -Force | Out-Null
+    }
+
+    # Create a safe temp copy before doing anything else
+    $safeName = [guid]::NewGuid().ToString() + $originalExt
+    $safePath = Join-Path $WorkingDirectory $safeName
+    Copy-Item -Path $InputPath -Destination $safePath -Force
+
+    # If no extension, guess and rename
+    if (-not $originalExt) {
+        $guessedExt = (New-Object ImageMagick.MagickImage($safePath)).Format.ToString().ToLower()
+        $safePathWithExt = "$safePath.$guessedExt"
+        Move-Item -Path $safePath -Destination $safePathWithExt -Force
+        $safePath = $safePathWithExt
     }
 
     # Detect type
-    $type = Get-ImageType $InputPath
+    $type = Get-ImageType $safePath
     $detectedAs = $type
-
     Write-Verbose "Detected image type: $type" -Verbose
 
     $preserveExt = @('jpg', 'jpeg', 'png') -contains $type
 
-    # Only convert and change extension if not in safe list
+    # Convert if needed
     if ($type -and -not $preserveExt) {
-        $Magick = New-Object ImageMagick.MagickImage($InputPath)
-        $convertedPath = [System.IO.Path]::ChangeExtension($InputPath, 'jpg')
+        $Magick = New-Object ImageMagick.MagickImage($safePath)
+        $convertedPath = [System.IO.Path]::ChangeExtension($safePath, 'jpg')
         $Magick.Format = [ImageMagick.MagickFormat]::Jpeg
         $Magick.Write($convertedPath)
-        $InputPath = $convertedPath
-        $type = 'jpg'  # Important: Update type since we converted
+        $safePath = $convertedPath
+        $type = 'jpg'
     }
 
-    # Normalize and shorten
-    $filename = [IO.Path]::GetFileName($InputPath)
-    $directory = [IO.Path]::GetDirectoryName($InputPath)
+    # Normalize name
+    $filename = [IO.Path]::GetFileName($safePath)
+    $directory = [IO.Path]::GetDirectoryName($safePath)
     $normalized = Normalize-String -InputString $filename -PreserveExtension -PreserveWhitespace
     $limited = Limit-FilenameLength -FullFilename $normalized -MaxLength $MaxLength -PreserveExtension
 
-    # Rebuild parts
     $extension = [IO.Path]::GetExtension($limited)
     $basename = [IO.Path]::GetFileNameWithoutExtension($limited)
 
     if (-not $basename) { $basename = "file" }
     if (-not $extension) { $extension = ".$type" }
-
     if ($basename.Length -lt 5) {
         $basename = $basename.PadRight(5, '_')
     }
@@ -63,15 +73,15 @@ function Normalize-And-ConvertImage {
     $finalFilename = "$basename$extension"
     $finalPath = Join-Path -Path $directory -ChildPath $finalFilename
 
-    if ($InputPath -ne $finalPath) {
-        Copy-Item -Path $InputPath -Destination $finalPath -Force
+    if ($safePath -ne $finalPath) {
+        Copy-Item -Path $safePath -Destination $finalPath -Force
     }
 
     return @{
-        FinalPath = $finalPath
-        Original  = $original
-        Extention = $extension
+        FinalPath  = $finalPath
+        Original   = $original
+        Extention  = $extension
         DetectedAS = $detectedAs
-        BaseName = $basename
+        BaseName   = $basename
     }
 }
