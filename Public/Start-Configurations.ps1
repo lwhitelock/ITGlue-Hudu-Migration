0e9c23d344205aee538a187d65ccf4e844837a61
Mendy Green <greenlighttec@users.noreply.github.com>
2025-08-11 22:59:46 -0400

    Update Start-Configurations.ps1
    Building fields for future configurations

---
+++ Public/Start-Configurations.ps1
@@ -0,0 +1,52 @@
+# New Fields with latest features
+<#
+        @{
+            label        = 'Configuration Type Name'
+            field_type   = 'ListSelect'
+            show_in_list = 'false'
+            position     = 15
+        },
+        @{
+            label        = 'Configuration Type Kind'
+            field_type   = 'ListSelect'
+            show_in_list = 'false'
+            position     = 16
+        },
+        @{
+            label        = 'Configuration Status Name'
+            field_type   = 'ListSelect'
+            show_in_list = 'false'
+            position     = 17
+        },
+        @{
+            label        = 'Manufacturer Name'
+            field_type   = 'ListSelect'
+            show_in_list = 'false'
+            position     = 18
+        },
+        @{
+            label        = 'Model ID'
+            field_type   = 'ListSelect'
+            show_in_list = 'false'
+            position     = 19
+        },
+        @{
+            label        = 'Operating System Name'
+            field_type   = 'ListSelect'
+            show_in_list = 'false'
+            position     = 20
+        },
+        @{
+            label        = 'Location Name'
+            field_type   = 'AssetLink'
+            show_in_list = 'false'
+            position     = 21
+        },
+        @{
+            label        = 'Model Name'
+            field_type   = 'ListSelect'
+            show_in_list = 'false'
+            position     = 22
+        }
+
+        #>
