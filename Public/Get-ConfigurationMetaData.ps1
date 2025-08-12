e6d21d17fdd92debdf7f13e4b13085dff3b55b8b
Mendy Green <greenlighttec@users.noreply.github.com>
2025-08-11 21:19:10 -0400

    Update Get-ConfigurationMetaData.ps1

--- Public/Get-ConfigurationMetaData.ps1
+++ Public/Get-ConfigurationMetaData.ps1
@@ -1,25 +1,25 @@
 ###### Configuration Meta Data #########
 
 # Types
-$ConfigurationTypesSelect = { Get-ITGlueConfigurationTypes -page_size 1000 -page_number $i }
+$ConfigurationTypesSelect = { (Get-ITGlueConfigurationTypes -page_size 1000 -page_number $i).data }
 $ITGConfigurationTypes = Import-ITGlueItems -ItemSelect $ConfigurationTypesSelect
 
 # Statuses
-$ConfigurationStatusesSelect = { Get-ITGlueConfigurationStatuses -page_size 1000 -page_number $i }
+$ConfigurationStatusesSelect = { (Get-ITGlueConfigurationStatuses -page_size 1000 -page_number $i).data }
 $ITGConfigurationStatuses = Import-ITGlueItems -ItemSelect $ConfigurationStatusesSelect
 
 # Manufacturers
-$ConfigurationManufacturersSelect = { Get-ITGlueManufacturers -page_size 1000 -page_number $i }
+$ConfigurationManufacturersSelect = { (Get-ITGlueManufacturers -page_size 1000 -page_number $i).data }
 $ITGConfigurationManufacturers = Import-ITGlueItems -ItemSelect $ConfigurationManufacturersSelect
 
 # Models
-$ConfigurationModelsSelect = { Get-ITGlueModels -page_size 1000 -page_number $i }
+$ConfigurationModelsSelect = { (Get-ITGlueModels -page_size 1000 -page_number $i).data }
 $ITGConfigurationModels = Import-ITGlueItems -ItemSelect $ConfigurationModelsSelect
 
 # Operating Systems
-$ConfigurationOsSelect = { Get-ITGlueOperatingSystems -page_size 1000 -page_number $i } 
+$ConfigurationOsSelect = { (Get-ITGlueOperatingSystems -page_size 1000 -page_number $i).data } 
 $ITGConfigurationOSes = Import-ITGlueItems -ItemSelect $ConfigurationOsSelect
 
 # Platforms
-$ConfigurationPlatformSelect = { Get-ITGluePlatforms -page_size 1000 -page_number $i }
+$ConfigurationPlatformSelect = { (Get-ITGluePlatforms -page_size 1000 -page_number $i).data }
 $ITGConfigurationPlatforms = Import-ITGlueItems -ItemSelect $ConfigurationPlatformSelect
