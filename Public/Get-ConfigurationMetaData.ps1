ce0476c683716b4b9892527e76c0306d9adc9d60
Mendy Green <greenlighttec@users.noreply.github.com>
2025-08-11 20:59:21 -0400

    Create Get-ConfigurationMetaData.ps1

---
+++ Public/Get-ConfigurationMetaData.ps1
@@ -0,0 +1,19 @@
+###### Configuration Meta Data #########
+
+# Types
+$ITGConfigurationTypes = Get-ITGlueConfigurationTypes
+
+# Statuses
+$ITGConfigurationStatuses = Get-ITGlueConfigurationStatuses
+
+# Manufacturers
+$ITGConfigurationManufacturers = Get-ITGlueManufacturers
+
+# Models
+$ITGConfigurationModels = Get-ITGlueModels
+
+# Operating Systems
+$ITGConfigurationOSes = Get-ITGlueOperatingSystems
+
+# Platforms
+$ITGConfigurationPlatforms = Get-ITGluePlatforms
