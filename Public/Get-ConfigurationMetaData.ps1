e4279a904f3338fbf2cc6f733b6bef19325c672a
Mendy Green <greenlighttec@users.noreply.github.com>
2025-08-11 22:22:08 -0400

    Update Get-ConfigurationMetaData.ps1
    Updated payload to work with the tempoary API Request

--- Public/Get-ConfigurationMetaData.ps1
+++ Public/Get-ConfigurationMetaData.ps1
@@ -17,35 +17,35 @@ function New-HuduList {
 # Types
 $ConfigurationTypesSelect = { (Get-ITGlueConfigurationTypes -page_size 1000 -page_number $i).data }
 $ITGConfigurationTypes = Import-ITGlueItems -ItemSelect $ConfigurationTypesSelect
-$NewHuduConfigurationTypeList = [pscustomobject]@{list = $ITGConfigurationTypes | Group-Object type | Select @{n='name';e={$_.name}}, @{n='list_items_attributes'; e={ $_.Group.attributes | select name } }}
+$NewHuduConfigurationTypeList = [pscustomobject]@{list = $ITGConfigurationTypes | Group-Object type | Select @{n='name';e={$_.name}}, @{n='list_items_attributes'; e={ $_.Group.attributes | Group-Object -Property name | select @{n='name'; e={ $_.name}} } }}
 $HuduListConfigurationTypes = New-HuduList $NewHuduConfigurationTypeList
 
 # Statuses
 $ConfigurationStatusesSelect = { (Get-ITGlueConfigurationStatuses -page_size 1000 -page_number $i).data }
 $ITGConfigurationStatuses = Import-ITGlueItems -ItemSelect $ConfigurationStatusesSelect
-$NewHuduConfigurationStatusList = [pscustomobject]@{list = $ITGConfigurationStatuses | Group-Object type | Select @{n='name';e={$_.name}}, @{n='list_items_attributes'; e={ $_.Group.attributes | select name } }}
+$NewHuduConfigurationStatusList = [pscustomobject]@{list = $ITGConfigurationStatuses | Group-Object type | Select @{n='name';e={$_.name}}, @{n='list_items_attributes'; e={ $_.Group.attributes | Group-Object -Property Name | select @{n='name'; e={$_.name}} } }}
 $HuduListConfigurationStatuses = New-HuduList $NewHuduConfigurationStatusList
 
 # Manufacturers
 $ConfigurationManufacturersSelect = { (Get-ITGlueManufacturers -page_size 1000 -page_number $i).data }
 $ITGConfigurationManufacturers = Import-ITGlueItems -ItemSelect $ConfigurationManufacturersSelect
-$NewHuduConfigurationManufacturerList = [pscustomobject]@{list = $ITGConfigurationManufacturers | Group-Object type | Select @{n='name';e={$_.name}}, @{n='list_items_attributes'; e={ $_.Group.attributes | select name } }}
+$NewHuduConfigurationManufacturerList = [pscustomobject]@{list = $ITGConfigurationManufacturers | Group-Object type | Select @{n='name';e={$_.name}}, @{n='list_items_attributes'; e={ $_.Group.attributes |Group-Object -Property Name | select @{n='name'; e={$_.name}} } }}
 $HuduListConfigurationManufacturerList = New-HuduList $NewHuduConfigurationManufacturerList
 
 # Models
 $ConfigurationModelsSelect = { (Get-ITGlueModels -page_size 1000 -page_number $i).data }
 $ITGConfigurationModels = Import-ITGlueItems -ItemSelect $ConfigurationModelsSelect
-$NewHuduConfigurationModelsList = [pscustomobject]@{list = $ITGConfigurationModels | Group-Object type | Select @{n='name';e={$_.name}}, @{n='list_items_attributes'; e={ $_.Group.attributes | select name } }}
+$NewHuduConfigurationModelsList = [pscustomobject]@{list = $ITGConfigurationModels | Group-Object type | Select @{n='name';e={$_.name}}, @{n='list_items_attributes'; e={ $_.Group.attributes |Group-Object -Property Name | select @{n='name'; e={$_.name}} } }}
 $HuduListConfigurationModelList = New-HuduList $NewHuduConfigurationModelsList
 
 # Operating Systems
 $ConfigurationOsSelect = { (Get-ITGlueOperatingSystems -page_size 1000 -page_number $i).data } 
 $ITGConfigurationOSes = Import-ITGlueItems -ItemSelect $ConfigurationOsSelect
-$NewHuduConfigurationOSesList = [pscustomobject]@{list = $ITGConfigurationOSes | Group-Object type | Select @{n='name';e={$_.name}}, @{n='list_items_attributes'; e={ $_.Group.attributes | select name } }}
+$NewHuduConfigurationOSesList = [pscustomobject]@{list = $ITGConfigurationOSes | Group-Object type | Select @{n='name';e={$_.name}}, @{n='list_items_attributes'; e={ $_.Group.attributes |Group-Object -Property Name | select @{n='name'; e={$_.name}} } }}
 $HuduListConfigurationOsList = New-HuduList $NewHuduConfigurationOSesList
 
 # Platforms
 $ConfigurationPlatformSelect = { (Get-ITGluePlatforms -page_size 1000 -page_number $i).data }
 $ITGConfigurationPlatforms = Import-ITGlueItems -ItemSelect $ConfigurationPlatformSelect
-$NewHuduConfigurationPlatformList = [pscustomobject]@{list = $ITGConfigurationPlatforms | Group-Object type | Select @{n='name';e={$_.name}}, @{n='list_items_attributes'; e={ $_.Group.attributes | select name } }}
+$NewHuduConfigurationPlatformList = [pscustomobject]@{list = $ITGConfigurationPlatforms | Group-Object type | Select @{n='name';e={$_.name}}, @{n='list_items_attributes'; e={ $_.Group.attributes |Group-Object -Property Name | select @{n='name'; e={$_.name}} } }}
 $HuduListConfigurationPlatformList = New-HuduList $NewHuduConfigurationPlatformList
