35ec488ae1ac224a7cbd788991a133bd9d4366bf
masonville17 <anoniboni9876@gmail.com>
2026-01-23 12:13:38 -0500

    I think that is good, will need a full runthrough or two to be sure, however

--- ITGlue-Hudu-Migration.ps1
+++ ITGlue-Hudu-Migration.ps1
@@ -133,7 +133,7 @@ if ($true -eq $allowSettingFlagsAndTypes){
         Write-Host "Flags and Flag Types aren't available in your hudu version. Upgrade your hudu instance and try again if it's a dealbreaker."
         $allowSettingFlagsAndTypes = $false
     } else {
-        $flagsResult = Get-UserFlagSetup; $ObjectFlagMap = $flagsResult.ObjectFlagMap ?? @{}; $allowSettingFlagsAndTypes = $flagsResult.AllowSettingFlags ?? $false; $passwordFlagCategories = $flagsResult.passwordFlagCategories ?? $false;
+        $flagsResult = Get-UserFlagSetup; $ObjectFlagMap = $flagsResult.ObjectFlagMap ?? @{}; $allowSettingFlagsAndTypes = $flagsResult.AllowSettingFlags ?? $false; $flagPasswordsByType = $flagsResult.passwordFlagCategories ?? $false;
     }
 }                
 # Check if we have a logs folder
@@ -2545,13 +2545,13 @@ if ($true -eq $allowSettingFlagsAndTypes){
     if (get-command -name Set-HapiErrorsDirectory -ErrorAction SilentlyContinue){try {Set-HapiErrorsDirectory -skipRetry $true} catch {}}
 
     $TaggingTargets = @{
-    "Company"        = $MatchedCompanies.HuduCompanyObject | Where-Object { $_.archived -ne $true }
-    "Articles"       = $MatchedArticles.HuduObject         | Where-Object { $_.archived -ne $true }
-    "Contacts"       = $MatchedContacts.HuduObject         | Where-Object { $_.archived -ne $true }
-    "Configurations" = $MatchedConfigurations.HuduObject   | Where-Object { $_.archived -ne $true }
-    "Locations"      = $MatchedLocations.HuduObject        | Where-Object { $_.archived -ne $true }
-    "Websites"       = $MatchedWebsites.HuduObject         | Where-Object { $_.archived -ne $true }
-    "Checklists"     = $MatchedChecklists.HuduProcedure    | Where-Object { $_.HuduProcedureTasks.count -gt 0 }
+        "Company"        = $MatchedCompanies.HuduCompanyObject | Where-Object { $_.archived -ne $true }
+        "Articles"       = $MatchedArticles.HuduObject         | Where-Object { $_.archived -ne $true }
+        "Contacts"       = $MatchedContacts.HuduObject         | Where-Object { $_.archived -ne $true }
+        "Configurations" = $MatchedConfigurations.HuduObject   | Where-Object { $_.archived -ne $true }
+        "Locations"      = $MatchedLocations.HuduObject        | Where-Object { $_.archived -ne $true }
+        "Websites"       = $MatchedWebsites.HuduObject         | Where-Object { $_.archived -ne $true }
+        "Checklists"     = $MatchedChecklists.HuduProcedure    | Where-Object { $_.HuduProcedureTasks.count -gt 0 }
     }
 
     if ($true -eq $flagPasswordsByType){
@@ -2563,28 +2563,28 @@ if ($true -eq $allowSettingFlagsAndTypes){
                 $passwordColor = Get-Random -InputObject @('red', 'crimson', 'scarlet', 'rot', 'karminrot', 'scharlachrot', 'rouge', 'cramoisi', 'écarlate', 'rosso', 'cremisi', 'scarlatto', 'rojo', 'carmesí', 'escarlata', 'blue', 'navy', 'blau', 'marineblau', 'bleu', 'bleu marine', 'blu', 'blu navy', 'azul', 'azul marino', 'green', 'lime', 'grün', 'limettengrün', 'vert', 'vert citron', 'verde', 'verde lime', 'verde lima', 'yellow', 'gold', 'gelb', 'jaune', 'or', 'giallo', 'oro', 'amarillo', 'purple', 'violet', 'lila', 'violett', 'pourpre', 'viola', 'porpora', 'púrpura', 'violeta', 'orange', 'arancione', 'naranja', 'light pink', 'pink', 'baby pink', 'hellrosa', 'rosa', 'rose clair', 'rose', 'rosa chiaro', 'rosa claro', 'light blue', 'baby blue', 'sky blue', 'hellblau', 'babyblau', 'himmelblau', 'bleu clair', 'bleu ciel', 'azzurro', 'blu chiaro', 'azul claro', 'celeste', 'light green', 'mint', 'hellgrün', 'mintgrün', 'vert clair', 'menthe', 'verde chiaro', 'menta', 'verde claro', 'light purple', 'lavender', 'helllila', 'lavendel', 'violet clair', 'lavande', 'viola chiaro', 'lavanda', 'morado claro', 'light orange', 'peach', 'hellorange', 'pfirsich', 'orange clair', 'pêche', 'arancione chiaro', 'pesca', 'naranja claro', 'melocotón', 'light yellow', 'cream', 'hellgelb', 'creme', 'jaune clair', 'crème', 'giallo chiaro', 'crema', 'amarillo claro', 'white', 'weiß', 'blanc', 'bianco', 'blanco', 'grey', 'gray', 'silver', 'grau', 'silber', 'gris', 'argent', 'grigio', 'argento', 'plateado', 'lightpink', 'lightblue', 'lightgreen', 'lightpurple', 'lightorange', 'lightyellow')
                 $passwordFlagMap[$category] = New-HuduFlagType -name "Password Category: $category" -color $passwordColor
                 $matchedpasswords | Where-Object { $_.ITGObject.attributes.'password-category-name' -ieq $category -and $_.HuduObject.id -and $_.HuduObject.id -gt 0 } | ForEach-Object {
-                    Set-HuduFlag -flagTypeId $passwordFlagMap[$category].id -FlagableType "Password" -FlagableID $_.HuduObject.id
+                    write-host "Setting password flag for $($category) with color $($passwordColor) on password $($_.HuduObject.name) $($_.HuduObject.id)"
+                    New-HuduFlag -FlagTypeId $passwordFlagMap[$category].id -Flagable_Type "Password" -flagable_id $_.HuduObject.id
                 }
             }
         }
     } else {
         write-host "Flagging passwords to singlular import tag"
         $TaggingTargets["Passwords"]= $MatchedPasswords.HuduObject        | Where-Object { $_.archived -ne $true }
-
     }
 
     foreach ($objectType in $TaggingTargets.GetEnumerator()) {
         $key   = $objectType.Key
         $items = @($objectType.Value)
 
-    if ($ObjectFlagMap -and $ObjectFlagMap.ContainsKey($key) -and $null -ne $ObjectFlagMap[$key]) {
-        Write-Host "Setting optional flags for $key ($($items.Count)) objects per user configuration"
-        $items | ForEach-Object {
-            Set-OptionalFlags -ObjectFlagMap $ObjectFlagMap -Object $_ -ObjectType $key
+        if ($ObjectFlagMap -and $ObjectFlagMap.ContainsKey($key) -and $null -ne $ObjectFlagMap[$key]) {
+            Write-Host "Setting optional flags for $key ($($items.Count)) objects per user configuration"
+            $items | ForEach-Object {
+                Set-OptionalFlags -ObjectFlagMap $ObjectFlagMap -Object $_ -ObjectType $key
+            }
+        } else {
+            Write-Host "No optional flags configured for $key, skipping"
         }
-    } else {
-        Write-Host "No optional flags configured for $key, skipping"
-    }
     }
     if (get-command -name Set-HapiErrorsDirectory -ErrorAction SilentlyContinue){try {Set-HapiErrorsDirectory -skipRetry $false} catch {}}
 } else {write-host "wrapup 9/9... Skipping optional flags and flag types..."}
