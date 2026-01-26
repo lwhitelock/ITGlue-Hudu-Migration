78c24471b5634c9ef5bdfe982576cbe8af5fa6af
masonville17 <anoniboni9876@gmail.com>
2026-01-26 11:12:31 -0500

    uncommenting non-company tagging targets

--- Public/Add-HuduFlagsFlagtypes.ps1
+++ Public/Add-HuduFlagsFlagtypes.ps1
@@ -2,12 +2,12 @@ if (get-command -name Set-HapiErrorsDirectory -ErrorAction SilentlyContinue){try
 
 $TaggingTargets = @{
     "Companies"       = $MatchedCompanies.HuduCompanyObject | Where-Object { $_.archived -ne $true }
-    # "Articles"       = $MatchedArticles.HuduObject         | Where-Object { $_.archived -ne $true }
-    # "Contacts"       = $MatchedContacts.HuduObject         | Where-Object { $_.archived -ne $true }
-    # "Configurations" = $MatchedConfigurations.HuduObject   | Where-Object { $_.archived -ne $true }
-    # "Locations"      = $MatchedLocations.HuduObject        | Where-Object { $_.archived -ne $true }
-    # "Websites"       = $MatchedWebsites.HuduObject         | Where-Object { $_.archived -ne $true }
-    # "Checklists"     = $MatchedChecklists.HuduProcedure    | Where-Object { $_.HuduProcedureTasks.count -gt 0 }
+    "Articles"       = $MatchedArticles.HuduObject         | Where-Object { $_.archived -ne $true }
+    "Contacts"       = $MatchedContacts.HuduObject         | Where-Object { $_.archived -ne $true }
+    "Configurations" = $MatchedConfigurations.HuduObject   | Where-Object { $_.archived -ne $true }
+    "Locations"      = $MatchedLocations.HuduObject        | Where-Object { $_.archived -ne $true }
+    "Websites"       = $MatchedWebsites.HuduObject         | Where-Object { $_.archived -ne $true }
+    "Checklists"     = $MatchedChecklists.HuduProcedure    | Where-Object { $_.HuduProcedureTasks.count -gt 0 }
 }
 
 if ($true -eq $flagPasswordsByType){
